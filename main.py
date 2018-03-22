078a7699c4ba5ec9887b053590874674f37b7c6c
iamcoleman <coleman.lyski@gmail.com>
2018-03-22 14:36:00 -0400

    sum daitz

--- main.py
+++ main.py
@@ -1,55 +1,60 @@
+# Python
+import pprint
+import sys
 # Libraries
 import tweepy
+import spotipy
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
 # Files
 from API.twitter_keys import getKeys
 
-#######################################
-# Get Twitter API keys
+##########################
+## Get Twitter API keys ##
+##########################
 access_token, access_secret, consumer_key, consumer_secret = getKeys()
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
-tiger = tweepy.API(auth)
-#######################################
+GTS = tweepy.API(auth)
+
+#############
+## Spotipy ##
+#############
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+search_str = 'Muse'
+result = sp.search(search_str)
+pprint.pprint(result)
 
 
+"""
 def reply(body, status):
 	user = status.author.screen_name
 	reply_id = status.id
 	juice = body
 	reply = "@"+user+" "+juice
-	tiger.update_status(reply, in_reply_to_status_id=reply_id)
+	GTS.update_status(reply, in_reply_to_status_id=reply_id)
 	print("Reply:")
 	print(reply)
 
 
-#######################################
-def replyTigerBack(status):
-	lower = status.text.lower()
-	if "tiger back" in lower:
-		print("Tweet includes tiger back...")
-		reply("He's back", status)
 
-#######################################
+###########################
+## STEAMING THE TIMELINE ##
+###########################
 
-
-#######################################
 class StreamListener(tweepy.StreamListener):
-
 	def on_status(self, status):
 		print("\nIncoming tweet by "+status.author.screen_name+":")
 		print(status.text)
-		replyTigerBack(status)
-
-
+		replyGTSBack(status)
 	def on_error(self, status_code):
 		if status_code == 420:
 			return False
-#######################################
 
-
-#######################################
 stream_listener = StreamListener()
 print("Stream Starting...\n")
-stream = tweepy.Stream(auth=tiger.auth, listener=stream_listener)
-stream.filter(track=["@is_tiger_back"])
-#######################################
+stream = tweepy.Stream(auth=GTS.auth, listener=stream_listener)
+stream.filter(track=["@GrabThisSong"])
+"""
